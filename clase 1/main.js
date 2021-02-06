4277213c0d29078dd28aabdcc4e1b394d99d6c25
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2021-02-06 14:39:08 -0400

    ejemplo de como declarar variables en js

---
+++ clase 1/main.js
@@ -0,0 +1,12 @@
+// Ejemplos practicos de como declarar 
+// variables en JS
+
+
+let miVariable = "Hola desde la variable";
+miVariable = 34.02
+
+var miOtraVariable = "Otra variable";
+miOtraVariable = 34.55
+
+console.log(miOtraVariable);
+console.log(miVariable);
