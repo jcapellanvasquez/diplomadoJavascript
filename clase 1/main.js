acb99bfdf011430359c1dcade5b25a3040a50961
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2021-02-06 14:58:12 -0400

    ejemplos con operadores

--- clase 1/main.js
+++ clase 1/main.js
@@ -14,3 +14,47 @@ console.log(miVariable);
 // Ejemplos de como declarar constantes en JS
 const MI_CONSTANTE = 6; 
 console.log(MI_CONSTANTE);
+
+// Tipos de datos
+
+// numerico
+// cadena de texto - string
+// bool
+
+let edad = -20;
+console.log(Math.abs(edad))
+
+let esMayorEdad = false; // true
+
+
+// Operadores Logicos
+
+// || => or
+// && => and
+// ! => not
+
+
+
+// Operadores de comparacion
+
+// == 
+// ===
+// <, <=
+// >, >=
+// != 
+// !==
+
+let var1 = "1";
+let var2 = 1;
+
+if (var1 == var2) {
+    console.log("son iguales")
+} else {
+    console.log("no son iguales")
+}
+
+if (var1 === var2) {
+    console.log("son iguales")
+} else {
+    console.log("no son iguales")
+}
