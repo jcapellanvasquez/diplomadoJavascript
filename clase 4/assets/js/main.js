3ee42f913c447bcc827137710f9c44cf9872b644
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2021-03-27 16:53:21 -0400

    clase 7 - uso del localstorage y uuid

--- clase 4/assets/js/main.js
+++ clase 4/assets/js/main.js
@@ -1,60 +1,139 @@
-const lista = document.getElementById("listado")
-const form = document.getElementById("mi-formulario")
-let tareas = [];
+"user strict";
 
-const renderizarLista = function() {
-    for (let index = 0; index < tareas.length; index++) {
-    
-        // Crear elementos con javascript de manera dinamica
-        const item = document.createElement("li")
-        item.classList.add('list-group-item');
-        
-         
-        
-        // Acceder al valor de la lista 
-        // mediante su indice
-        const p1 = document.createElement("p")
-        p1.innerText = tareas[index].titulo
+window.addEventListener('load', () => {
 
-        const p2 = document.createElement("p")
-        p2.innerText = tareas[index].descripcion
-
-        const button = document.createElement("button");
-        button.classList.add("btn","btn-light")
-        button.innerText = "Seleccionar"
-        button.dataset.id = index
-        button.addEventListener('click', seleccionarTarea)
-
-        item.append(p1)
-        item.append(p2)
-        item.append(button)
-    
-        // Agregar elementos a un elemento contenedor
-        lista.append(item)
-    }
-}
-
-const accionFormulario = function(event) {
-    event.preventDefault()
-
-    // Agregar un elemento al final 
-    // de la lista o array
-    let tarea = {
-        titulo: event.target.titulo.value,
-        descripcion: event.target.descripcion.value,
+    const lista = document.getElementById("listado")
+    const form = document.getElementById("mi-formulario")
+    const getTareas = () => {
+        let tareas = JSON.parse(localStorage.getItem("tareas"))
+        if (!tareas) {
+            localStorage.setItem("tareas", JSON.stringify([]))
+            tareas = JSON.parse(localStorage.getItem("tareas"))
+        }
+        return tareas
     }
 
-    tareas.push(tarea)
+    let tareas = getTareas()
 
-    // renderizar la lista
+    const renderizarLista = function () {
+        limpiarLista()
+
+        for (let index = 0; index < tareas.length; index++) {
+
+            // limpar la lista visual
+
+
+            // Crear elementos con javascript de manera dinamica
+            const item = document.createElement("li")
+            item.classList.add('list-group-item');
+            item.dataset.id = index
+
+
+
+            // Acceder al valor de la lista 
+            // mediante su indice
+            const p1 = document.createElement("p")
+            p1.innerText = tareas[index].titulo
+
+            const p2 = document.createElement("p")
+            p2.innerText = tareas[index].descripcion
+
+            const button = document.createElement("button");
+            button.classList.add("btn", "btn-light", "me-3")
+            button.innerText = "Borrar"
+            button.dataset.id = tareas[index].id
+            button.addEventListener('click', borrarTarea)
+            p2.innerText = tareas[index].descripcion
+
+            const button1 = document.createElement("button");
+            button1.classList.add("btn", "btn-light")
+            button1.innerText = "Editar"
+            button1.dataset.id = index
+            button1.addEventListener('click', editarTarea)
+
+
+            item.append(p1)
+            item.append(p2)
+            item.append(button)
+            item.append(button1)
+
+            // Agregar elementos a un elemento contenedor
+            lista.append(item)
+        }
+    }
+
+    const limpiarLista = function () {
+        let hijos = document.querySelectorAll("li")
+        if (hijos.length > 0) {
+            for (let i = 0; i < hijos.length; i++) {
+                lista.removeChild(hijos[i])
+            }
+        }
+    }
+
+
+    const actulizarStorage = () => localStorage.setItem("tareas", JSON.stringify(tareas))
+
+    const accionFormulario = function (event) {
+        event.preventDefault()
+
+        // Agregar un elemento al final 
+        // de la lista o array
+        let tarea;
+        if (event.target.id.value) {
+            tarea = tareas.find(function (tar) {
+                return tar.id === event.target.id.value
+            })
+
+            tarea.titulo = event.target.titulo.value;
+            tarea.descripcion = event.target.descripcion.value
+
+            tareas = tareas.filter(function (tar) {
+                return tar.id !== tarea.id
+            });
+
+
+        } else {
+            tarea = {
+                titulo: event.target.titulo.value,
+                descripcion: event.target.descripcion.value,
+                id: uuidv4()
+            }
+        }
+
+        tareas.push(tarea)
+        actulizarStorage()
+        form.reset()
+
+        // renderizar la lista
+        renderizarLista()
+
+    }
+
+    const borrarTarea = function (event) {
+        let respuesta = confirm("Desea eliminar este registro");
+        let tarea;
+
+        if (respuesta) {
+            tareas = tareas.filter(function (tar) {
+                return tar.id !== event.target.dataset.id
+            });
+            actulizarStorage()
+            renderizarLista()
+        }
+    }
+
+    const editarTarea = function (event) {
+        let id = event.target.dataset.id
+        let tarea = tareas[id]
+        form.id.value = tarea.id
+        form.titulo.value = tarea.titulo
+        form.descripcion.value = tarea.descripcion
+    }
+
+    form.addEventListener("submit", accionFormulario)
+
     renderizarLista()
 
-}
 
-const seleccionarTarea = function(event) {
-    console.log(event.target.dataset.id)
-
-    console.log(tareas[event.target.dataset.id])
-}
-
-form.addEventListener("submit", accionFormulario)
+})
